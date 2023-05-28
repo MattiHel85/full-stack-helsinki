91eae2006fd072119bbb50d35ff254b741e48113
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-05-28 13:06:11 +0300

    commit yesterdays changes

--- part1/tasks/clickhandler/src/App.js
+++ part1/tasks/clickhandler/src/App.js
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setClicks({...clicks, left: clicks.left + 1})
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setClicks({...clicks, right: clicks.right + 1})
+  }
 
   return (
       <>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>
+        {clicks.left}
+        <button onClick={handleLeftClick}>
           left
         </button>
-        <button onClick={() => setRight(right + 1)}>
+        <button onClick={handleRightClick}>
           right
         </button>
-        {right}
+        {clicks.right}
+        <p>{allClicks.join(' ')}</p>
       </>
   );
 }
