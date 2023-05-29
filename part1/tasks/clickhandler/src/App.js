b6c2695707bce1e38b4d78a08a8cc5f9124e1800
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-05-29 13:58:56 +0300

    add unicafe

--- part1/tasks/clickhandler/src/App.js
+++ part1/tasks/clickhandler/src/App.js
@@ -1,32 +1,48 @@
 import React, { useState } from 'react';
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setClicks({...clicks, left: clicks.left + 1})
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setClicks({...clicks, right: clicks.right + 1})
+    const updatedRight = right + 1
+    setRight(updatedRight)
   }
 
   return (
       <>
-        {clicks.left}
+        {left} 
         <button onClick={handleLeftClick}>
           left
         </button>
         <button onClick={handleRightClick}>
           right
-        </button>
-        {clicks.right}
-        <p>{allClicks.join(' ')}</p>
+        </button> 
+        {right}
+        <History allClicks={allClicks} />
       </>
   );
 }
