0ada60f973ec4168148ca279bf05337cad56eaa3
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-06-25 12:00:24 +0300

    add Header and Content components to Course component

--- part2/courseinfo-pt2/src/components/Content.js
+++ part2/courseinfo-pt2/src/components/Content.js
@@ -7,6 +7,7 @@ function Content(props) {
       <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
       <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
       <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+      <Part part={props.course.parts[3].name} exercises={props.course.parts[3].exercises} />
     </>
   )
 }
