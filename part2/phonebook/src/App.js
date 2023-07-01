1dd368bd7254a7678c76a25d949117168c625a17
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-07-01 13:05:14 +0300

    start phonebook exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+
+const App = () => {
+  const [person, setPersons] = useState([
+    {
+      name: 'Arto Hellas'
+    }
+  ])
+
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>
+        Phonebook
+      </h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type='submit'>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>
+        Numbers
+      </h2>
+      <div>
+
+      </div>
+      <div>
+        debug: {newName}
+      </div>
+    </div>
+  );
+}
+
+export default App;
