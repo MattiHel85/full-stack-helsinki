5f31e3dcd809bb5774573120c1b8b22be2233420
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-07-24 11:48:44 +0300

    Complete ex. 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,33 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Search from './components/Search'
 import AddPerson from './components/AddPerson'
 import Person from './components/Person'
 import Header from './components/Header'
 
 const App = () => {
+  
+  
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('');
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(res => {
+    const persons = res.data
+    console.log(persons)
+    setPersons(persons)
+    })
+  }, [])
+
+  
 
   const addPerson = (e) => {
     e.preventDefault()
