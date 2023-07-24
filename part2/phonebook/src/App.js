f6513bb0ba50284b5c4f737117365cf215634fb5
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-07-24 17:37:31 +0300

    Complete ex 2.12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,12 +8,7 @@ import Header from './components/Header'
 const App = () => {
   
   
-  const [persons, setPersons] = useState([
-    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -41,7 +36,9 @@ const App = () => {
         name: newName,
         number: newNumber
       };
-      setPersons([...persons, newPerson]);
+      axios.post('http://localhost:3001/persons', newPerson).then((res) => {
+        setPersons([...persons, res.data]); // Update the state with the new person from the server
+      });
       setNewName('');
       setNewNumber('');
     }
